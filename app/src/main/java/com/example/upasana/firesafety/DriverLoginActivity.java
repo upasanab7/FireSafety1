b2cc752c19101d82f0f011398b5785970224738c
upasanab7 <40449364+upasanab7@users.noreply.github.com>
2018-06-21 11:48:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/upasana/firesafety/DriverLoginActivity.java
@@ -0,0 +1,84 @@
+package com.example.upasana.firesafety;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class DriverLoginActivity extends AppCompatActivity {
+    private EditText memail, mname;
+    private Button  mregister;
+
+    private FirebaseAuth mauth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_login);
+
+        mauth= FirebaseAuth.getInstance();
+
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if(user!= null){
+                    Intent intent = new Intent(DriverLoginActivity.this, MapsActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+            }
+        };
+        memail= findViewById(R.id.email);
+        mname= findViewById(R.id.name);
+
+
+        mregister= findViewById(R.id.register);
+
+        mregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email = memail.getText().toString();
+                final String password = mname.getText().toString();
+                mauth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(DriverLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(DriverLoginActivity.this, "sign up error", Toast.LENGTH_SHORT).show();;
+                        }else{
+                            String user_id = mauth.getCurrentUser().getUid();
+                            DatabaseReference current_user_db  = FirebaseDatabase.getInstance().getReference().child("Users").child("users").child(user_id);
+                            current_user_db.setValue(true);
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mauth.addAuthStateListener(firebaseAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mauth.removeAuthStateListener(firebaseAuthListener);
+    }
+}
---
+++ app/src/main/java/com/example/upasana/firesafety/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.upasana.firesafety;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mDriver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDriver= (Button) findViewById(R.id.driver);
+
+        mDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this, DriverLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+    }
+}
